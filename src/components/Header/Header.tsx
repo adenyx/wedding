5d7d0215d9928fa52524837378b844983ea69b59
Denis Avramenko <d.avramenko@21vek.by>
2025-11-12 21:44:02 +0300

    first block added

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <div className="header-container">
+      <h1 className="header-title">Свадебный Вестник</h1>
+
+      <div className="header-separator" />
+
+      <div className="header-description">
+        <p className="header-invitation">ПРИГЛАШЕНИЕ НА СВАДЬБУ</p>
+
+        <p className="header-date">10 ЯНВАРЯ 2026</p>
+      </div>
+
+      <div className="header-separator-small" />
+
+      <p className="header-conclusion-title">АЛИНА & ДЕНИС ЖЕНЯТСЯ!</p>
+
+      <div className="header-separator" />
+
+      <p className="heading-3">ГЛАВНАЯ СВАДЬБА ГОДА</p>
+
+      <div className="header-separator-small" />
+
+      <img src="src/assets/header.jpg" alt="header image" className="header-img" />
+    </div>
+  )
+}
