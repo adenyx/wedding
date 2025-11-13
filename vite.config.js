f44c3b9cb8b2de62cc755e732fc8d54ad33be610
Denis Avramenko <d.avramenko@21vek.by>
2025-11-13 07:55:01 +0300

    fix assets for prod build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wedding/'
+  base: '/wedding/',
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg']
 })
